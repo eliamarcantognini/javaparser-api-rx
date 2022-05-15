9c883273bd2af3a04fc0006cffef9a1d3989b85a
Elia Marcantognini <elia.marcantognini@gmail.com>
2022-05-15 19:51:36 +0200

    Add test

--- src/main/java/TestProjectAnalyzer.java
+++ src/main/java/TestProjectAnalyzer.java
@@ -1,27 +1,33 @@
 import lib.ProjectAnalyzer;
+import lib.rx.ReactiveProjectAnalyzer;
 
 import java.util.concurrent.Future;
 
 public class TestProjectAnalyzer {
 
 
-    private final static String INTERFACE = "";
-    private final static String CLASS = "";
-    private final static String PACKAGE = "";
+    private final static String INTERFACE = "src/main/java/lib/reports/interfaces/ClassReport.java";
+    private final static String CLASS = "src/main/java/controller/AnalysisController.java";
+    private final static String PACKAGE = "src/main/java/lib/reports/";
     private final static String PROJECT = "";
 
     public static void main(String[] args) {
-        ProjectAnalyzer projectAnalyzer;
+        ProjectAnalyzer projectAnalyzer = new ReactiveProjectAnalyzer();
+//        testClassReport(projectAnalyzer, CLASS);
+//        testInterfaceReport(projectAnalyzer, INTERFACE);
+        testPackageReport(projectAnalyzer, PACKAGE);
     }
 
     private static void testInterfaceReport(ProjectAnalyzer projectAnalyzer, final String interfaceToAnalyze) {
-
+        projectAnalyzer.getInterfaceReport(interfaceToAnalyze).subscribe(e -> System.out.println(e.toString()));
     }
 
     private static void testClassReport(ProjectAnalyzer projectAnalyzer, final String classToAnalyze) {
+        projectAnalyzer.getClassReport(classToAnalyze).subscribe(e -> System.out.println(e.toString()));
     }
 
     private static void testPackageReport(ProjectAnalyzer projectAnalyzer, final String packageToAnalyze) {
+        projectAnalyzer.getPackageReport(packageToAnalyze).subscribe(e -> System.out.println(e.toString()));
     }
 
     public static void testProjectReport(ProjectAnalyzer projectAnalyzer, final String projectToAnalyze) {
